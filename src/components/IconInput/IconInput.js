a732121fd823b7d186ac71d5e4f116b0f5556ddc
John Bernard <38332081+Flandre-X@users.noreply.github.com>
2021-03-23 14:33:27 -0500

    Solution

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,84 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const SIZES = {
+  small: {
+    "--height": 24 / 16 + "rem",
+    "--borderWidth": 1 + "px",
+    "--fontSize": 14 / 16 + "rem",
+    "--iconSize": 14 + "px",
+  },
+  large: {
+    "--height": 36 / 16 + "rem",
+    "--borderWidth": 2 + "px",
+    "--fontSize": 18 / 16 + "rem",
+    "--iconSize": 18 + "px",
+  },
 };
+
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  const validSizes = Object.keys(SIZES);
+  if (!validSizes.includes(size)) {
+    throw new Error(`Invalid size: ${size}`);
+  }
+
+  const styles = {
+    "--width": `${width}px`,
+    ...SIZES[size],
+  };
+
+  return (
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <TextInput id="icon-input" placeholder={placeholder} style={styles} />
+      <IconWrapper style={{ "--size": styles["--iconSize"] }}>
+        <Icon id={icon} size={styles["--iconSize"]} />
+      </IconWrapper>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.label`
+  position: relative;
+  display: block;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  width: var(--size);
+  height: var(--size);
+  pointer-events: none;
+`;
+
+const TextInput = styled.input`
+  width: var(--width);
+  height: var(--height);
+  padding-left: var(--height);
+  border: none;
+  border-bottom: var(--borderWidth) solid ${COLORS.black};
+  outline-offset: 2px;
+  font-size: var(--fontSize);
+  font-weight: 700;
+  color: inherit;
+
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+`;
 
 export default IconInput;
--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,11 +1,70 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled, { css } from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const SIZES = {
+  small: {
+    "--height": "8px",
+    "--padding": "0",
+    "--borderRadius": "4px",
+  },
+  medium: {
+    "--height": "12px",
+    "--padding": "0",
+    "--borderRadius": "4px",
+  },
+  large: {
+    "--height": "16px",
+    "--padding": "4px",
+    "--borderRadius": "8px",
+  },
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const thing = Object.keys(SIZES);
+  if (!thing.includes(size)) {
+    throw new Error(`Unknown size passed to ProgressBar: ${size}`);
+  }
+
+  const styles = SIZES[size];
+
+  return (
+    <BaseProgressBar
+      style={styles}
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+    >
+      <BaseProgressBarFill value={value} />
+    </BaseProgressBar>
+  );
 };
+
+const BaseProgressBar = styled.div`
+  height: var(--height);
+  padding: var(--padding);
+  border-radius: var(--borderRadius);
+
+  width: 370px;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const BaseProgressBarFill = styled.div`
+  ${({ value }) => css`
+    height: 100%;
+    width: ${value}%;
+    border-radius: 4px;
+    background: ${COLORS.primary};
+
+    ${value != 100 &&
+    `
+      border-top-right-radius: 0;
+      border-bottom-right-radius: 0;
+    `}
+  `}
+`;
 
 export default ProgressBar;
